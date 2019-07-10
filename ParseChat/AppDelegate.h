df37df01303331d3f8cbe30da048a93d8c8cc496
Carlos Power <powercarlos25@fb.com>
2019-07-10 10:35:48 -0700

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by powercarlos25 on 7/10/19.
+//  Copyright © 2019 powercarlos25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
